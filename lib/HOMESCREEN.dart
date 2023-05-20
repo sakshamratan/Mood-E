c40f32aac0e0d9b527a2c590b7cc136caab3b5ff
sakshamratan <sakshamratan7597740255@gmail.com>
2023-05-21 01:49:32 +0530

    update 1

--- lib/HOMESCREEN.dart
+++ lib/HOMESCREEN.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
 import 'dart:typed_data';
-import 'dart:ui';
 
 import 'package:flutter/material.dart';
-
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -40,188 +40,308 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             Text(
               'Select Image',
-              style:
-              TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
             ),
             SizedBox(
               height: 20.0,
             ),
             ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor:
-                    MaterialStateProperty.all(Colors.green),
-                    padding:
-                    MaterialStateProperty.all(const EdgeInsets.all(20)),
-                    textStyle: MaterialStateProperty.all(const TextStyle(
-                        fontSize: 14, color: Colors.white))),
-                onPressed: () async {
-                  selectImage();
-                },
-                child: const Text('Select')),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.green),
+                padding: MaterialStateProperty.all(const EdgeInsets.all(20)),
+                textStyle: MaterialStateProperty.all(const TextStyle(
+                  fontSize: 14,
+                  color: Colors.white,
+                )),
+              ),
+              onPressed: () {
+                selectImage();
+              },
+              child: const Text('Select'),
+            ),
             const SizedBox(height: 10),
           ],
         ),
       ),
     );
   }
-  Future selectImage() {
-    return showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return Dialog(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0)), //this right here
-            child: Container(
-              height: 150,
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Column(
-                  children: [
-                    Text(
-                      'Select Image From !',
-                      style: TextStyle(
-                          fontSize: 18.0, fontWeight: FontWeight.bold),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        GestureDetector(
-                          onTap: () async {
-                            selectedImagePath = await selectImageFromGallery();
-                            print('Image_Path:-');
-                            print(selectedImagePath);
-                            if (selectedImagePath != '') {
-                              Navigator.pop(context);
-                              setState(() {});
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                                content: Text("No Image Selected !"),
-                              ));
-                            }
-                          },
-                          child: Card(
-                              elevation: 5,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Column(
-                                  children: [
-                                    Image.asset(
-                                      'assets/images/gallery.png',
-                                      height: 60,
-                                      width: 60,
-                                    ),
-                                    Text('Gallery'),
-                                  ],
-                                ),
-                              )),
-                        ),
-                        GestureDetector(
-                          onTap: () async {
-                            selectedImagePath = await selectImageFromCamera();
-                            print('Image_Path:-');
-                            print(selectedImagePath);
 
-                            if (selectedImagePath != '') {
-                              Navigator.pop(context);
-                              setState(() {});
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                                content: Text("No Image Captured !"),
-                              ));
-                            }
-                          },
-                          child: Card(
-                              elevation: 5,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Column(
-                                  children: [
-                                    Image.asset(
-                                      'assets/images/camera.png',
-                                      height: 60,
-                                      width: 60,
-                                    ),
-                                    Text('Camera'),
-                                  ],
+  Future selectImage() async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Container(
+            height: 150,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Select Image From!',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                        onTap: () async {
+                          selectedImagePath = await selectImageFromGallery();
+                          if (selectedImagePath != '') {
+                            Navigator.pop(context);
+                            navigateToNextPage(selectedImagePath);
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("No Image Selected!"),
+                              ),
+                            );
+                          }
+                        },
+                        child: Card(
+                          elevation: 5,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                  'assets/images/gallery.png',
+                                  height: 60,
+                                  width: 60,
                                 ),
-                              )),
+                                Text('Gallery'),
+                              ],
+                            ),
+                          ),
                         ),
-                      ],
-                    )
-                  ],
-                ),
+                      ),
+                      GestureDetector(
+                        onTap: () async {
+                          selectedImagePath = await selectImageFromCamera();
+                          if (selectedImagePath != '') {
+                            Navigator.pop(context);
+                            navigateToNextPage(selectedImagePath);
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("No Image Captured!"),
+                              ),
+                            );
+                          }
+                        },
+                        child: Card(
+                          elevation: 5,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                  'assets/images/camera.png',
+                                  height: 60,
+                                  width: 60,
+                                ),
+                                Text('Camera'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
               ),
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 
-  selectImageFromGallery() async {
-    XFile? file = await ImagePicker()
-        .pickImage(source: ImageSource.gallery, imageQuality: 10);
-    if (file != null) {
-      return file.path;
+  Future<String> selectImageFromGallery() async {
+    final imagePicker = ImagePicker();
+    final pickedImage = await imagePicker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 10,
+    );
+
+    if (pickedImage != null) {
+      return pickedImage.path;
     } else {
       return '';
     }
   }
 
-  //
-  selectImageFromCamera() async {
-    XFile? file = await ImagePicker()
-        .pickImage(source: ImageSource.camera, imageQuality: 10);
-    if (file != null) {
-      return file.path;
+  Future<String> selectImageFromCamera() async {
+    final imagePicker = ImagePicker();
+    final pickedImage = await imagePicker.pickImage(
+      source: ImageSource.camera,
+      imageQuality: 10,
+    );
+
+    if (pickedImage != null) {
+      final imagePath = await copyImageToDocumentsDirectory(File(pickedImage.path));
+      return imagePath;
     } else {
       return '';
     }
   }
-}
-decodeImage(Uint8List bytes) {
-}
 
-Future<String> _processImage(File imageFile) async {
-  // Decode the image using the Image package
-  final Uint8List bytes = await imageFile.readAsBytes();
-  final image = decodeImage(bytes);
-
-  // Resize the image to 224x224 using the Image package
-  final resizedImage = copyResizeCropSquare(image, 224);
-
-  // Save the resized image to a temporary file
-  final tempDir = await getTemporaryDirectory();
-  final tempPath = '${tempDir.path}/${DateTime.now().millisecondsSinceEpoch}.jpg';
-  File(tempPath).writeAsBytesSync((resizedImage));
-
-  return tempPath;
-}
-
-copyResizeCropSquare(image, int i) {
-}
-
-
-
-Future selectImage(Null Function() param0) async {
-  XFile? image = await ImagePicker().pickImage(
-    source: ImageSource.gallery,
-    imageQuality: 10,
-  );
-
-  if (image != null) {
-    File imageFile = File(image.path);
-    String documentsDirectory = (await getApplicationDocumentsDirectory()).path;
-    String fileName = DateTime.now().millisecondsSinceEpoch.toString();
-    String imagePath = '$documentsDirectory/$fileName.jpg';
+  Future<String> copyImageToDocumentsDirectory(File imageFile) async {
+    final documentsDirectory = await getApplicationDocumentsDirectory();
+    final fileName = DateTime.now().millisecondsSinceEpoch.toString();
+    final imagePath = '${documentsDirectory.path}/$fileName.jpg';
 
     try {
-      // Copy the selected image to the app's documents directory
       await imageFile.copy(imagePath);
-      selectImage(() {
-        var selectedImagePath = imagePath;
-      });
+      return imagePath;
     } catch (e) {
       print(e);
+      return '';
     }
+  }
+
+  void navigateToNextPage(String imagePath) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => NextPage(imagePath: imagePath),
+      ),
+    );
   }
 }
 
+class NextPage extends StatefulWidget {
+  final String imagePath;
+
+  const NextPage({required this.imagePath, Key? key}) : super(key: key);
+
+  @override
+  State<NextPage> createState() => _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  String output = '';
+  bool isModelReady = false;
+  late Interpreter _interpreter;
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel();
+  }
+// modellllllllllllllllllllllllll//
+  void loadModel() async {
+    var Tflite;
+    await Tflite.loadModel(
+      model: 'assets/labels/model.tflite',
+      labels: 'assets/labels/labels.csv',
+    );
+    setState(() {
+      isModelReady = true;
+    });
+  }
+
+  Future<File> getFileFromAsset(String assetPath) async {
+    final byteData = await rootBundle.load(assetPath);
+    final file = File('${(await getTemporaryDirectory()).path}/$assetPath');
+    await file.writeAsBytes(byteData.buffer.asUint8List());
+
+    return file;
+  }
+
+  void runModel() async {
+    if (widget.imagePath.isNotEmpty && isModelReady) {
+      final Uint8List imageBytes = await File(widget.imagePath).readAsBytes();
+      var Tflite;
+      var predictions = await Tflite.runModelOnImage(
+        path: widget.imagePath,
+        imageMean: 0.0,
+        imageStd: 255.0,
+        numResults: 2,
+        threshold: 0.2,
+      );
+      if (predictions != null && predictions.isNotEmpty) {
+        setState(() {
+          output = predictions[0]['label'];
+        });
+      }
+    }
+  }
+
+  Uint8List preprocessImage(Uint8List imageBytes, List<int> inputShape) {
+    // Perform any necessary preprocessing on the image bytes
+    // Resize, normalize, etc.
+    // Example preprocessing code:
+    // ...
+
+    // Return the preprocessed image as Uint8List
+    return imageBytes;
+  }
+
+  String postprocessOutput(List<dynamic> output) {
+    // Perform any necessary postprocessing on the model output
+    // Convert logits to probabilities, apply thresholding, etc.
+    // Example postprocessing code:
+    // ...
+
+    // Return the final label
+    return 'Label';
+  }
+
+  @override
+  void dispose() {
+    _interpreter.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow.shade800,
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.yellow.shade800,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.file(
+                File(widget.imagePath),
+                height: 300,
+                width: 300,
+                fit: BoxFit.fill,
+              ),
+              ElevatedButton(
+                onPressed: runModel,
+                child: Text('GET MUSIC'),
+                style: ButtonStyle(
+                  backgroundColor:
+                  MaterialStateProperty.all<Color>(Colors.green),
+                ),
+              ),
+              SizedBox(height: 10),
+              CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.blue,
+                child: IconButton(
+                  color: Colors.white,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(Icons.arrow_back),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_startup/HOMESCREEN.dart';
-import 'package:flutter_startup/home_page.dart';
-import 'package:flutter_startup/takephoto_page.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_startup/next_page.dart';
 
-void main() async {
-  runApp(const MyApp());
-  void _launchSpotifyPlaylist(String playlistUri) async {
-    String url = 'spotify:playlist:$playlistUri';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
 
-}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -28,7 +16,18 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const HomeScreen(),
+
         debugShowCheckedModeBanner: false);
   }
+}
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: 'HOMESCREEN',
+    routes: {'HOMESCREEN':(context) => HomeScreen(),
+      'next_page':(context) => MyLogin(), },
+
+
+  ));
+
 }
